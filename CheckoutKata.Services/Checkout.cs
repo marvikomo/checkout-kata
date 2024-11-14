5560507374339a40e3a5dac2e44d4357b37a5356
marvikomo <marvikomo@gmail.com>
2024-11-14 01:03:26 +0100

    feat: Implement basic item pricing

--- CheckoutKata.Services/Checkout.cs
+++ CheckoutKata.Services/Checkout.cs
@@ -6,12 +6,20 @@ namespace CheckoutKata.Services
 {
     public class Checkout : ICheckout
     {
-        public Checkout() {}
+        private readonly Dictionary<string, int> _scannedItems = new();
+        public Checkout() {
 
-        public void Scan(string item) {}
+        }
+
+        public void Scan(string item) {
+            if(!_scannedItems.ContainsKey(item))
+                _scannedItems[item] = 0;
+            _scannedItems[item]++;
+            
+        }
 
         public int GetTotalPrice() {
-           return 0;
+           return _scannedItems.GetValueOrDefault("A", 0) * 50;
         }
     }
 }
