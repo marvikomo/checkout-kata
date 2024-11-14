5d299f329290792be7307be7eb98701426142e0b
marvikomo <marvikomo@gmail.com>
2024-11-14 04:34:52 +0100

    refractor: Update tests to pass Pricing rules to the constructor

--- CheckoutKata.Services.Tests/CheckoutUnitTests.cs
+++ CheckoutKata.Services.Tests/CheckoutUnitTests.cs
@@ -1,14 +1,26 @@
 using System;
+using CheckoutKata.Services.Model;
 
 
 namespace CheckoutKata.Services.Tests
 {
     public class CheckoutUnitTests
     {
+        private static IEnumerable<PricingRule> CreateDefaultRules()
+        {
+            return new[]
+            {
+            new PricingRule("A", 50, new SpecialOffer(3, 130)),
+            new PricingRule("B", 30, new SpecialOffer(2, 45)),
+            new PricingRule("C", 20),
+            new PricingRule("D", 15)
+        };
+        }
+
         [Fact]
         public void WhenNoItems_TotalShouldBeZero()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(CreateDefaultRules());
             Assert.Equal(0, checkout.GetTotalPrice());
         }
 
@@ -19,21 +31,22 @@ namespace CheckoutKata.Services.Tests
         [InlineData("D", 15)]
         public void WhenSingleItem_ShouldReturnUnitPrice(string item, int expectedPrice)
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(CreateDefaultRules());
             checkout.Scan(item);
             Assert.Equal(expectedPrice, checkout.GetTotalPrice());
         }
 
         [Fact]
-        public void WhenInvalidItem_ShouldThrowException(){
-            var checkout = new Checkout();
-            Assert.Throws<ArgumentException>( () => checkout.Scan("X"));
+        public void WhenInvalidItem_ShouldThrowException()
+        {
+            var checkout = new Checkout(CreateDefaultRules());
+            Assert.Throws<ArgumentException>(() => checkout.Scan("X"));
         }
 
         [Fact]
         public void WhenThreeAs_ShouldApplySpecialPrice()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(CreateDefaultRules());
             checkout.Scan("A");
             checkout.Scan("A");
             checkout.Scan("A");
@@ -45,7 +58,7 @@ namespace CheckoutKata.Services.Tests
         [Fact]
         public void WhenTwoBs_ShouldApplySpecialPrice()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(CreateDefaultRules());
             checkout.Scan("B");
             checkout.Scan("B");
             Assert.Equal(45, checkout.GetTotalPrice());
@@ -58,18 +71,18 @@ namespace CheckoutKata.Services.Tests
         [InlineData(new[] { "B", "B", "B" }, 75)]   // 2B special + 1B normal
         public void WhenMultipleItems_ShouldApplySpecialPrice(string[] items, int expectedTotal)
         {
-        var checkout = new Checkout();
-        foreach (var item in items)
-        {
-            checkout.Scan(item);
-        }
+            var checkout = new Checkout(CreateDefaultRules());
+            foreach (var item in items)
+            {
+                checkout.Scan(item);
+            }
             Assert.Equal(expectedTotal, checkout.GetTotalPrice());
         }
 
-       
-       
 
-        
+
+
+
 
     }
 }
