cae3e7e7800bf3f48b69588d6b042ddca3ed02d4
marvikomo <marvikomo@gmail.com>
2024-11-14 02:38:23 +0100

    feat: implement calculation in SimplePriceRule

--- CheckoutKata.Services/PricingRules/SimplePriceRule.cs
+++ CheckoutKata.Services/PricingRules/SimplePriceRule.cs
@@ -6,14 +6,16 @@ namespace CheckoutKata.Services.PricingRules
 {
     public class SimplePriceRule : IPricingRule
     {
+        private readonly int unitPrice;
         public string Item { get; }
         public SimplePriceRule(string item, int unitPrice)
         {
            Item = item;
+           this.unitPrice = unitPrice;
         }
         public int CalculateTotal(int quantity)
         {
-            throw new NotImplementedException();
+           return quantity * unitPrice;
         }
     }
 }
