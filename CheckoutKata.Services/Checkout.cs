42580b837a7aa4cef9ef13712ba34b2305cf8e84
marvikomo <marvikomo@gmail.com>
2024-11-14 04:41:58 +0100

    refractor: Refractor checkout to make use of price rules passed through constructor

--- CheckoutKata.Services/Checkout.cs
+++ CheckoutKata.Services/Checkout.cs
@@ -1,63 +1,41 @@
 using System;
-
+using CheckoutKata.Services.Factories;
 using CheckoutKata.Services.Interfaces;
+using CheckoutKata.Services.Model;
 
 namespace CheckoutKata.Services
 {
     public class Checkout : ICheckout
     {
-        private readonly Dictionary<string, int> _scannedItems = new();
+        private readonly Dictionary<string, IPricingRule> pricingRules;
+        private readonly Dictionary<string, int> scannedItems;
 
-         private readonly Dictionary<string, int> _prices = new()
+        public Checkout(IEnumerable<PricingRule> rules)
         {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
+            if (rules == null)
+                throw new ArgumentNullException(nameof(rules));
 
-        private readonly Dictionary<string, (int quantity, int specialPrice)> _specialOffers = new()
-        {
-            { "A", (3, 130) },
-            { "B", (2, 45) }
-        };
-
-        public Checkout() {
+            pricingRules = PriceRuleFactory.CreatePricingRules(rules);
+            scannedItems = new Dictionary<string, int>();
 
         }
 
-        public void Scan(string item) {
-            if(!_prices.ContainsKey(item))
-            {
+        public void Scan(string item)
+        {
+            if (!pricingRules.ContainsKey(item))
                 throw new ArgumentException($"Invalid item: {item}");
-            }
-            if(!_scannedItems.ContainsKey(item))
-                _scannedItems[item] = 0;
-            _scannedItems[item]++;
-            
+
+            if (!scannedItems.ContainsKey(item))
+                scannedItems[item] = 0;
+
+            scannedItems[item]++;
+
         }
 
-        public int GetTotalPrice() {
-          int total = 0;
-          foreach (var item in _scannedItems)
-          {
-            string sku = item.Key;
-            int quantity = item.Value;
-
-            if(_specialOffers.ContainsKey(sku))
-            {
-                var (specialQuantity, specialPrice) = _specialOffers[sku];
-                int specialGroups = quantity / specialQuantity;
-                int remainingItems = quantity % specialQuantity;
-                total += (specialGroups * specialPrice) + (remainingItems * _prices[sku]);
-            }
-            else
-            {
-                 total += quantity * _prices[sku];
-            }
-            
-          }
-          return total;
+        public int GetTotalPrice()
+        {
+            return scannedItems.Sum(item =>
+              pricingRules[item.Key].CalculateTotal(item.Value));
         }
     }
 }
