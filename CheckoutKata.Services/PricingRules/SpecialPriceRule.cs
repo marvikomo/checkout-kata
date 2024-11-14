43dd08a0c5c1d880f6f5de6a2948584ae8ac0925
marvikomo <marvikomo@gmail.com>
2024-11-14 02:45:56 +0100

    refractor SpecialPriceRule to pass

--- CheckoutKata.Services/PricingRules/SpecialPriceRule.cs
+++ CheckoutKata.Services/PricingRules/SpecialPriceRule.cs
@@ -6,6 +6,11 @@ namespace CheckoutKata.Services.PricingRules
 {
     public class SpecialPriceRule : IPricingRule
     {
+        public string Item { get; }
+        public SpecialPriceRule(string item, int unitPrice, int specialQuantity, int specialPrice)
+        {
+          Item = item;
+        }
         public int CalculateTotal(int quantity)
         {
             throw new NotImplementedException();
