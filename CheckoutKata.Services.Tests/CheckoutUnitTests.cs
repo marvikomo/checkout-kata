77a19e976f7e3939b91b65c6865284018501cd13
marvikomo <marvikomo@gmail.com>
2024-11-14 01:08:41 +0100

    test: Add tests for multiple items

--- CheckoutKata.Services.Tests/CheckoutUnitTests.cs
+++ CheckoutKata.Services.Tests/CheckoutUnitTests.cs
@@ -14,12 +14,19 @@ namespace CheckoutKata.Services.Tests
 
         [Theory]
         [InlineData("A", 50)]
+        [InlineData("B", 30)]
+        [InlineData("C", 20)]
+        [InlineData("D", 15)]
         public void WhenSingleItem_ShouldReturnUnitPrice(string item, int expectedPrice)
         {
             var checkout = new Checkout();
             checkout.Scan(item);
             Assert.Equal(expectedPrice, checkout.GetTotalPrice());
         }
+
+       
+       
+
         
 
     }
