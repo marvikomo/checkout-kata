120c227ddb265a4aed36f3167a01c1552fa637cf
marvikomo <marvikomo@gmail.com>
2024-11-14 02:31:49 +0100

    refractor SimplePriceRule to pass

--- CheckoutKata.Services/PricingRules/SimplePriceRule.cs
+++ CheckoutKata.Services/PricingRules/SimplePriceRule.cs
@@ -6,6 +6,11 @@ namespace CheckoutKata.Services.PricingRules
 {
     public class SimplePriceRule : IPricingRule
     {
+        public string Item { get; }
+        public SimplePriceRule(string item, int unitPrice)
+        {
+           Item = item;
+        }
         public int CalculateTotal(int quantity)
         {
             throw new NotImplementedException();
