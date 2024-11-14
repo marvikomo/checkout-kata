873c975264e6328c01e74b08fde2d42c2cbb0dda
marvikomo <marvikomo@gmail.com>
2024-11-14 03:15:21 +0100

    test: Add test for PriceRuleFactory creating special price rule

--- CheckoutKata.Services.Tests/PriceRulesFactory/PriceRuleFactoryTest.cs
+++ CheckoutKata.Services.Tests/PriceRulesFactory/PriceRuleFactoryTest.cs
@@ -24,5 +24,20 @@ namespace CheckoutKata.Services.Tests.PriceRulesFactory
             Assert.Equal(50, priceRule.CalculateTotal(1));
         }
 
+        [Fact]
+        public void Create_WithSpecialOffer_ShouldReturnSpecialPricer()
+        {
+            var rules = new[] {
+            new PricingRule("A", 50, new SpecialOffer(3, 130))
+            };
+
+            var pricerRules = PriceRuleFactory.Create(rules);
+
+            var pricerRule = pricerRules.Single();
+            Assert.IsType<SpecialPriceRule>(pricerRule);
+            Assert.Equal("A", pricerRule.Item);
+            Assert.Equal(130, pricerRule.CalculateTotal(3));
+        }
+
     }
 }
