53335a134bd73f0750557797669a6ce7abd89ebe
marvikomo <marvikomo@gmail.com>
2024-11-14 02:33:51 +0100

    test: Add calculation test for SimplePriceRule

--- CheckoutKata.Services.Tests/PricingRules/SimplePriceRuleTest.cs
+++ CheckoutKata.Services.Tests/PricingRules/SimplePriceRuleTest.cs
@@ -12,5 +12,16 @@ namespace CheckoutKata.Services.Tests.PricingRules
             var pricer = new SimplePriceRule("A", 50);
             Assert.Equal("A", pricer.Item);
         }
+
+        [Theory]
+        [InlineData(0, 0)]    
+        [InlineData(1, 50)] 
+        [InlineData(2, 100)] 
+        public void CalculateTotal_ShouldReturnCorrectAmount(int quantity, int expected)
+        {
+            var pricer = new SimplePriceRule("A", 50);
+            var total = pricer.CalculateTotal(quantity);
+            Assert.Equal(expected, total);
+        }
     }
 }
